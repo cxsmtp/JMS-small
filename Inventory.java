24f3d5077d5283203c1fe8962625bc11d2750a52
cxsmtp <67365405+cxsmtp@users.noreply.github.com>
2026-04-22 17:03:00 +0400

    Checkmarx Bot - Auto Pull Request

--- Inventory.java
+++ Inventory.java
@@ -159,21 +159,32 @@ class Inventory extends JFrame implements ActionListener
                 String sstone_number = stone_number.getText();
        	        String sdetails = details.getText();
 
-	 	String query =" INSERT INTO Inventory(Style_ID,Vendor_ID,In_Date,Gold,Gold_wt,Stone_Type,Stone_Weight,Stone_numbers,Details) VALUES ('"+sstyle_id+"','"+sVendor_id+"','"+sin_date+"','"+sgold_cr+"','"+sgold_wt+"','"+sstone_type+"','"+sstone_wt+"','"+sstone_number+"','"+sdetails+"')";
-                //System.out.println("Query ="+query);
+	 	// Use PreparedStatement to prevent SQL injection
+	 	String query = "INSERT INTO Inventory(Style_ID,Vendor_ID,In_Date,Gold,Gold_wt,Stone_Type,Stone_Weight,Stone_numbers,Details) VALUES (?,?,?,?,?,?,?,?,?)";
 		try
 	        {
         		Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
 		        con = DriverManager.getConnection("jdbc:odbc:JMS");
-			stmt = con.createStatement();
-			int result = stmt.executeUpdate ( query );
+			// Use PreparedStatement instead of Statement for parameterized query
+			PreparedStatement pstmt = con.prepareStatement(query);
+			pstmt.setString(1, sstyle_id);
+			pstmt.setString(2, sVendor_id);
+			pstmt.setString(3, sin_date);
+			pstmt.setString(4, sgold_cr);
+			pstmt.setString(5, sgold_wt);
+			pstmt.setString(6, sstone_type);
+			pstmt.setString(7, sstone_wt);
+			pstmt.setString(8, sstone_number);
+			pstmt.setString(9, sdetails);
+			int result = pstmt.executeUpdate();
+			pstmt.close();
 		}
 		catch(Exception ae)
               	{
               		ae.printStackTrace();
 	        }
-		
-		
-		//Addenq1 p = new Addenq1();		
+
+
+		//Addenq1 p = new Addenq1();
 	}
 }
